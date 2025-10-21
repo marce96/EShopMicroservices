b9485b4140c133df01f7cb489699fa3290934254
marcela.pozo <marcel14navarro@gmail.com>
2025-10-21 12:25:47 -0600

    Add DI initial structure

--- src/services/Ordering/Ordering.API/Program.cs
+++ src/services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 var app = builder.Build();
+
+app.UseApiServices();
 
 // configure the http request pipeline
 
