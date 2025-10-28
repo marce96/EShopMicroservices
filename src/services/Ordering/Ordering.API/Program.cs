5afd1a559bddf5be223b3c47ecf484fa862db55e
marcela.pozo <marcel14navarro@gmail.com>
2025-10-28 13:03:04 -0600

    Ordering Infrastructure

--- src/services/Ordering/Ordering.API/Program.cs
+++ src/services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +14,12 @@ builder.Services
 
 var app = builder.Build();
 
+// configure the http request pipeline
 app.UseApiServices();
 
-// configure the http request pipeline
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
