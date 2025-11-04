ee03aa17de900de0c303fbd991830e8e26fbdc4e
marcela.pozo <marcel14navarro@gmail.com>
2025-11-04 10:27:15 -0600

    Ordering API

---
+++ src/services/Ordering/Ordering.API/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Ordering.API;
+global using Ordering.Application;
+global using Ordering.Infrastructure;
+global using Ordering.Infrastructure.Data.Extensions;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
--- src/services/Ordering/Ordering.API/Program.cs
+++ src/services/Ordering/Ordering.API/Program.cs
@@ -1,8 +1,3 @@
-using Ordering.API;
-using Ordering.Application;
-using Ordering.Infrastructure;
-using Ordering.Infrastructure.Data.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
@@ -10,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices();
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
